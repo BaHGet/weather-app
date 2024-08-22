6ae6cd14fbc6db9e419e6a01ac278a9a2b5649a4
BaHGet <abosama150@gmail.com>
2024-08-23 01:42:57 +0300

    weather card component to render weather details

---
+++ src/app/weather-card/weather-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrl: './weather-card.component.css'
+})
+export class WeatherCardComponent {
+  cloud = '/assets/images/cloud.png';
+  windSpeed = '/assets/images/wind-speed.png';
+  humidity = '/assets/images/humidity.png';
+  cloudIcon = '/assets/images/cloud-icon.png';
+  sunSet = '/assets/images/sun-set.png';
+  sunRise = '/assets/images/sun-rise.png';
+}
