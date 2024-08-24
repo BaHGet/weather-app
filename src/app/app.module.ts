44e7cf820a921a39d60e998eb3fc53d59e8f5c60
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2024-08-24 10:37:10 +0300

    add Temperature, Diraction to Module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,21 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { routes } from "./app-routing.module";
 import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { HttpClientModule } from "@angular/common/http";
+import { Temperature } from "./Pipes/Temperature.pipe";
+import { Diraction } from "./Pipes/Diraction.pipe";
 
 @NgModule({
     declarations: [
         AppComponent,
         WeatherCardComponent,
+        Temperature,
+        Diraction
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        HttpClientModule,
         RouterModule.forRoot(routes)
     ],
     providers: [],
